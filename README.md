308b5f049bc398dd298787c0d219b0e7f02852f0
Haseo Chen <tpxcer@outlook.com>
2023-11-09 14:39:06 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# fio
-fio 磁盘性能测试脚本，模拟 CrystalDiskMark 。
+# 一、介绍
+该项目是为了方便在我的课程[《TrueNAS SCALE 终极教程》](https://www.bilibili.com/cheese/play/ss6060)中进行各种磁盘布局的性能测试开发的，模拟了CrystalDiskMark几个测试项目。在 fio 参数上有任何疑问或者需求可以给我提 issue ，欢迎 pr。
+
+# 二、项目依赖
+
+目前仅支持类 Unix 环境，依赖软件[fio](https://github.com/axboe/fio)和[jq](https://github.com/jqlang/jq)
+
+## 2.1 Debian/Ubuntu 依赖安装
+
+```bash
+sudo apt install fio jq -y
+```
+
+# 2.2 CentOS 系列依赖安装
+
+```bash
+sudo yum install fio jq -y
+```
+
+# 三、使用
+
+直接下载`fio_mark.sh`文件，或者克隆到本地
+
+```bash
+# 克隆项目
+git clone https://github.com/bihell/fio
+```
+
+开始测试，注意把`/mnt/test`替换为你要测试的路径。
+
+> 程序每个测试项单任务文件大小为1G，循环跑5次。
+>
+> 注意：参数设定上要比CrystalDiskMark严格些，如果你跑 ssd 可能会占用很多时间，一般默认测试即可。
+
+```Bash
+# 默认测试：只要输入测试路径即可，对应CrystalDiskMark的默认测试（耗时短）
+TrueNas-➜  ~ sudo bash fio_mark.sh /mnt/Stripe1
+[sudo] password for admin:
+Executing Default test:
+                    Read [MB/s]     Read [IOPS]    Write [MB/s]    Write [IOPS]]
+     SEQ1M-Q8T1            2234            2227             408             406
+    RND4K-Q32T1             133           34084              15            4022
+     SEQ1M-Q1T1            2283            2282             864             863
+     RND4K-Q1T1             129           33113              79           20331
+     
+# SSD 测试：只要多输入个参数`ssd`，对应CrystalDiskMark的 NVME 测试（耗时长）
+TrueNas-➜  ~ sudo bash fio_mark.sh /mnt/test ssd
+
+# 一起测：输入`all`参数则CrystalDiskMark的 NVME 和 SDD 测试一起测（耗时很长）
+TrueNas-➜  ~ sudo bash fio_mark.sh /mnt/test all
+
+# 如果只是想看看能不能跑通，请增加`test`参数
+TrueNas-➜  ~ sudo bash fio_mark.sh /mnt/test test
+```
+
+
+
---
+++ fio_mark.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# 脚本项目地址：https://github.com/bihell/fio
+# 该项目是为了方便在我的课程《TrueNAS SCALE 终极教程》进行各种磁盘布局的性能测试开发的，课程地址：https://www.bilibili.com/cheese/play/ss6060
+# 模拟了 CrystalDiskMark 的测试方式，方便使用参考对比。如果对 fio 参数或者其他方面有疑问，可以到项目上提 issue 给我。脚本的使用方法以及更新可以看项目的描述。
+
+# 默认参数
+LOOP=5
+SIZE=1g
+RUNTIME=60s
+TYPE=default
+
+read_arg() {
+    case "$1" in
+    test)
+        LOOP=1
+        SIZE=10m
+        ;;
+    all)
+        TYPE=all
+        ;;
+    ssd)
+        TYPE=ssd
+        ;;
+    *)
+        if [ -d "$1" ]; then
+            TEST_DIR=$1
+        fi
+        ;;
+    esac
+}
+
+# 检查参数
+for var in "$@"; do
+    read_arg $var
+done
+if [ -z "$TEST_DIR" ]; then
+    echo "测试路径错误或未输入！"
+    exit 1
+fi
+
+# 测试命令
+fio_general="sudo fio --directory=$TEST_DIR --size=$SIZE --loops=$LOOP --ramp_time=2s --ioengine=libaio --direct=1 --stonewall --verify=0 --group_reporting=1 --output-format=json --output \"$TEST_DIR/fio_result\" \
+--name=read,RND4K-Q1T1 --rw=randread --iodepth=1 --bs=4k --numjobs=1 \
+--name=write,RND4K-Q1T1 --rw=randwrite --iodepth=1 --bs=4k --numjobs=1 \
+--name=read,SEQ1M-Q8T1 --rw=read --iodepth=8 --bs=1M --numjobs=1 \
+--name=write,SEQ1M-Q8T1 --rw=write --iodepth=8 --bs=1M --numjobs=1"
+fio_default="--name=read,SEQ1M-Q1T1 --rw=read --iodepth=1 --bs=1M --numjobs=1 \
+             --name=write,SEQ1M-Q1T1 --rw=write --iodepth=1 --bs=1M --numjobs=1 \
+             --name=read,RND4K-Q32T1 --rw=randread --iodepth=32 --bs=4k --numjobs=1 \
+             --name=write,RND4K-Q32T1 --rw=randwrite --iodepth=32 --bs=4k --numjobs=1"
+fio_nvme="--name=read,SEQ128K-Q32T1 --rw=read --iodepth=32 --bs=128k --numjobs=1 \
+          --name=write,SEQ128K-Q32T1 --rw=write --iodepth=32 --bs=128k --numjobs=1 \
+          --name=read,RND4K-Q32T16 --rw=randread --iodepth=32 --bs=4k --numjobs=16 \
+          --name=write,RND4K-Q32T16 --rw=randwrite --iodepth=32 --bs=4k --numjobs=16"
+
+# JQ 查询
+jq_general='def read_bw(name): [.jobs[] | select(.jobname==name).read.bw] | add / 1024 |floor|tostring;
+            def write_bw(name): [.jobs[] | select(.jobname==name).write.bw] | add / 1024 | floor|tostring;
+            def read_iops(name): [.jobs[] | select(.jobname==name).read.iops] | add | floor|tostring;
+            def write_iops(name): [.jobs[] | select(.jobname==name).write.iops] | add | floor|tostring;
+            def job_summary(name): name+","+"read,bw"+","+read_bw(name),name+","+"write,bw"+","+write_bw(name),name+","+"read,io"+","+read_iops(name),name+","+"write,io"+","+write_iops(name);
+            job_summary("read,RND4K-Q1T1"),job_summary("write,RND4K-Q1T1"),
+            job_summary("read,SEQ1M-Q8T1"),job_summary("write,SEQ1M-Q8T1")'
+jq_default=',job_summary("read,SEQ1M-Q1T1"),job_summary("write,SEQ1M-Q1T1"),
+            job_summary("read,RND4K-Q32T1"),job_summary("write,RND4K-Q32T1")'
+jq_nvme=',job_summary("read,SEQ128K-Q32T1"),job_summary("write,SEQ128K-Q32T1"),
+         job_summary("read,RND4K-Q32T16"),job_summary("write,RND4K-Q32T16")'
+
+# 根据类型执行相应的命令段
+if [[ "$TYPE" == "default" ]]; then
+    command="$fio_general $fio_default"
+    query="$jq_general $jq_default"
+    echo "Executing Default test:"
+    eval "$command"
+elif [[ "$TYPE" == "ssd" ]]; then
+    command="$fio_general $fio_nvme"
+    query="$jq_general $jq_nvme"
+    echo "Executing SSD test:"
+    eval "$command"
+elif [[ "$TYPE" == "all" ]]; then
+    command="$fio_general $fio_default $fio_nvme"
+    query="$jq_general $jq_default $jq_nvme"
+    echo "Executing ALL test:"
+    eval "$command"
+fi
+
+# 解析输出
+jq -r "$query" "$TEST_DIR/fio_result" | awk -F',' '{
+    key = $2;
+    if ($1 == "read" && $3 == "read") {
+        if ($4 == "bw") read_bw[key] = $5;
+        if ($4 == "io") read_io[key] = $5;
+    }
+    if ($1 == "write" && $3 == "write") {
+        if ($4 == "bw") write_bw[key] = $5;
+        if ($4 == "io") write_io[key] = $5;
+    }
+}
+END {
+    printf("%15s %15s %15s %15s %15s\n", "", "Read [MB/s]", "Read [IOPS]", "Write [MB/s]", "Write [IOPS]");
+    for (key in read_bw) {
+        printf("%15s %15s %15s %15s %15s\n", key, read_bw[key], read_io[key], write_bw[key], write_io[key]);
+    }
+}'
+
+# 清理文件
+sudo rm $TEST_DIR/read,* $TEST_DIR/write,* $TEST_DIR/fio_result
